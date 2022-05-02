c0b9604bb12b445554a9b860dde319eceaa8bd16
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-05-01 23:03:07 -0300

    aaaaa

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,20 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    artist: "Peep",
+    title: "Gym Class",
+    release_year: 2000,
+    formats: ["CD", "8T", "LP"]
+  }
+
+];
